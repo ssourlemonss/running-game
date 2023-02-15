ea376bb491c7b951dba8191fb5f24a92146391da
ssourlemonss <117904442+ssourlemonss@users.noreply.github.com>
2023-02-15 20:30:11 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var s1, s2
+function setup() {
+  createCanvas(400,400);
+  s1 = new Student("Devika",13,7)
+  s1.display()
+  s2 = new Student("Devika number 2",12,6)
+  s2.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
